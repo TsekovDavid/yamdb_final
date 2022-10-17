1f15f5519d4c1181d06bd0602d9360e067bfd49f
Давид <david@MacBook-Air-Kristina.local>
2022-10-17 16:20:57 +0300

    sort imports

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator, UniqueValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title, User
 from reviews.validators import validate_username
 
